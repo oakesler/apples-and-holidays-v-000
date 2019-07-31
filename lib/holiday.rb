9563aa8aba5a8bcf3ecea082046097f2e44e6121
Olivia Kesler <oakesler@protonmail.ch>
2019-07-31 06:50:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,8 +80,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_array = [ ]
   holiday_hash.each do |season, holiday|
     holiday.each do |day, item|
-      thing = holiday_hash[day]
-      binding.pry
+      if item.include?("BBQ")
+        holiday_array << day
     end
   end
 end
